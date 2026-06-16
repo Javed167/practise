0526dcb146f7ba237436a28fc8017ad31819fd96
Javed167 <javed1320143@gmail.com>
2026-06-16 20:17:01 +0300

    calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,27 @@
-marks = int(input("Enter your marks: "))
-if marks >= 90:
-    print("Grade: A")
-elif marks >= 80:
-    print("Grade: B")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+choice = input("Choose operation (1/2/3/4): ")
+
+if choice == "1":
+    print("Result =", num1 + num2)
+
+elif choice == "2":
+    print("Result =", num1 - num2)
+
+elif choice == "3":
+    print("Result =", num1 * num2)
+
+elif choice == "4":
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Error: Division by zero is not allowed.")
+
 else:
-    print("Grade: C")
+    print("Invalid choice!")
